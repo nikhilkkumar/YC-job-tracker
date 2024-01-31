4bf0fbdea64689f2969bd6ed8121d3f363583066
Nikhil Kumar <nikhil@Nikhils-MacBook-Pro.local>
2024-01-30 17:13:11 -0800

    replace initial code with gpsread test code

--- YC_job_tracker.py
+++ YC_job_tracker.py
@@ -1,33 +1,8 @@
-import requests
-from bs4 import BeautifulSoup
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
 
-# Google Sheets setup
-scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('your_credentials_file.json', scope)
-client = gspread.authorize(creds)
-sheet = client.open('your_spreadsheet_name').sheet1
+keys = 'yc-job-scraper-027dfd285b0d.json'
+gc = gspread.service_account(keys)
 
-def scrape_job_data(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    
-    # Extract data using BeautifulSoup based on the HTML structure of the Y Combinator job board
-    company_name = # extract company name
-    role = # extract role
-    location = # extract location
-    pay = # extract pay
-    equity = # extract equity
-    description = # extract description
+sh = gc.open("YC Startups and roles")
 
-    return [company_name, role, location, pay, equity, description]
-
-def update_sheet(data):
-    # Append data to the Google Sheet
-    sheet.append_row(data)
-
-# Example usage
-job_url = 'https://www.ycombinator.com/companies/coris/jobs/2j8IBtf-bizops'
-job_data = scrape_job_data(job_url)
-update_sheet(job_data)
+print(sh.sheet1.get('A1'))
