a040c15ea19dde5bfa0090809a1aa065a9588284
Nikhil Kumar <nikhil@Nikhils-MacBook-Pro.local>
2024-01-30 23:18:44 -0800

    add BeautifulSoup package and parse company and role

--- YC_job_tracker.py
+++ YC_job_tracker.py
@@ -1,8 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
 import gspread
 
-keys = 'yc-job-scraper-027dfd285b0d.json'
-gc = gspread.service_account(keys)
+#google spreadsheet setup
+keys = 'yc-job-scraper-027dfd285b0d.json' #authentication
+client = gspread.service_account(keys) #service account using keys to authenticate
+sh = client.open("YC Startups and roles") #opening up spreadsheet
 
-sh = gc.open("YC Startups and roles")
+def scrape_job_data(url):
+    response = requests.get(url) #webpage retrival
+    soup = BeautifulSoup(response.text, 'html.parser') #returns HTML code
 
-print(sh.sheet1.get('A1'))
+    company_name = soup.find("h2").get_text()
+    role = soup.find("h1",class_="ycdc-section-title mb-2").get_text()
+    
+    return role
+
+print(scrape_job_data("https://www.ycombinator.com/companies/coris/jobs/2j8IBtf-bizops"))
